18e66751828790eb440e2c3b4336e0efa8f711bd
jianshishen <runningjelly@gmail.com>
2019-06-01 21:55:21 +1000

    Second Update

--- count-areas.py
+++ count-areas.py
@@ -1,14 +1,21 @@
+import time
 import count
 import argparse
+
+start_time = time.time()
 
 # Setup arguments of this code
 parser = argparse.ArgumentParser(description='Counting coloured areas in an image. The default shape is 256*256')
 parser.add_argument('file', type=str, help='Input a binary file')
 parser.add_argument('--shape', type=str, help='Input height and width')
 args = parser.parse_args()
+c=count.Count()
 
 # Print the amounts of coloured areas
 if (args.shape):
-    print (count.apply(args.file,int(args.shape.split(',')[0]),int(args.shape.split(',')[1])))
+    print (c.apply(args.file,int(args.shape.split(',')[0]),int(args.shape.split(',')[1])))
 else:
-    print (count.apply(args.file,256,256))
+    print (c.apply(args.file,256,256))
+
+elapsed_time = time.time() - start_time
+print('Duration:',time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
--- count.py
+++ count.py
@@ -1,89 +1,115 @@
+import os
 import numpy as np
 from collections import deque
+from multiprocessing import Pool
 
-FLAG = -2
-INIT = -1
+class Count(object):
+   def __init__(self) :
+      self.height=None
+      self.width=None
+      self.sorted_pixels=None
+      self.sorted_image=None
+      self.labels=None
 
-"""Get nearest pixels on the vertical axis and the horizontal axis
+   """Get nearest pixels on the vertical axis and the horizontal axis
 
-Args:
-   height: int "The height of the image"
-   width: int "The width of the image"
-   pixel: 2*1 array "The coordinate of the pixel"
+   Args:
+      height: int "The height of the image"
+      width: int "The width of the image"
+      pixel: 1*2 array "The coordinate of the pixel"
 
-Returns:
-   Numpy array
-"""
-def get_neighbours(height, width, pixel):
-   return np.unique(np.array([[pixel[0],max(0,pixel[1]-1)],[pixel[0],min(width-1,pixel[1]+1)],[max(0, pixel[0] - 1),pixel[1]],[min(height-1, pixel[0] + 1),pixel[1]]]),axis=0)
+   Returns:
+      Set
+   """
+   def get_neighbours(self,height, width, pixel):
+      # return np.unique(np.array([[pixel[0],max(0,pixel[1]-1)],[pixel[0],min(width-1,pixel[1]+1)],[max(0, pixel[0] - 1),pixel[1]],[min(height-1, pixel[0] + 1),pixel[1]]]),axis=0)
+      return set([(pixel[0],max(0,pixel[1]-1)),(pixel[0],min(width-1,pixel[1]+1)),(max(0, pixel[0] - 1),pixel[1]),(min(height-1, pixel[0] + 1),pixel[1])])
 
-"""Get the amount of the area having the same gray scale
+   """Count numbers of area with the same gray scale
 
-Args:
-   file: bin file "The image represented by a binary file"
-   height: int "The height of the image"
-   width: int "The width of the image"
+   Args:
+      index: 1*2 array "Two boundaries where pixels with the same gray scale starts and ends"
 
-Returns:
-   1*256 Array
-"""
-def apply(file,height,width):
-   current_label = 0
-   flag = False
-   fifo = deque()
-   scale_indices = []
-   current_scale = 0
-   start_index = 0
-   result=[0 for x in range(256)]
-   image= np.fromfile(file,dtype='uint8')
-   total = height * width
+   Returns:
+      1*256 array
+   """
+   def count_areas(self,index):
+      FLAG = -2
+      CHECK=1
+      start_index = index[0]
+      stop_index=index[1]
+      fifo = deque()
+      result=[0 for x in range(256)]
 
-   # Create a canva with an identical scale covered by an intial value
-   labels = np.full((height, width), INIT, np.int32)
-
-   reshaped_image = image.reshape(total)
-   # Create coordinates of every pixel.
-   pixels = np.mgrid[0:height, 0:width].reshape(2, -1).T
-   # Coordinates of neighbour pixels for each pixel.
-   # neighbours = np.array([get_neighbours(height, width, p) for p in pixels]).reshape(height,width)
-
-   # Sort coordinates by gray scale
-   indices = np.argsort(reshaped_image)
-   sorted_image = reshaped_image[indices]
-   sorted_pixels = pixels[indices]
-
-   # Get the boundaries of pixels with different gray scales.
-   for i in range(total):
-      if sorted_image[i] > current_scale:
-         current_scale=sorted_image[i]
-         scale_indices.append(i)
-   scale_indices.append(total)
-
-   # Pick up areas with the same gray scale
-   for stop_index in scale_indices:
       # Put flags on all pixels with current scale.
-      for p in sorted_pixels[start_index:stop_index]:
-         labels[p[0], p[1]] = FLAG
+      for p in self.sorted_pixels[start_index:stop_index]:
+         self.labels[p[0], p[1]] = FLAG
 
       # Label all pixels with current scale within the same area.
-      for p in sorted_pixels[start_index:stop_index]:
-         if labels[p[0], p[1]] == FLAG:
+      for p in self.sorted_pixels[start_index:stop_index]:
+         if self.labels[p[0], p[1]] != FLAG:continue
+         # Count as one new area with the current gray scale
+         result[int(self.sorted_image[start_index])]+=1
+         fifo.append(p)
+         self.labels[p[0], p[1]] = CHECK
+         
+         # Find if there is a new neighbour with the current scale
+         while fifo:
+            q = fifo.popleft()
+            # for r in neighbours[q[0],q[1]]:
+            for r in self.get_neighbours(self.height,self.width,q):
+               if self.labels[r[0], r[1]] != FLAG:continue
+               fifo.append(r)
+               self.labels[r[0], r[1]] = CHECK
+      return result
 
-            # Count as one new area with the current gray scale
-            result[int(sorted_image[start_index])]+=1
-            current_label += 1
-            fifo.append(p)
-            labels[p[0], p[1]] = current_label
-            
-            # Find if there is a new neighbour with the current scale
-            while fifo:
-               q = fifo.popleft()
-               # for r in neighbours[q[0],q[1]]:
-               for r in get_neighbours(height,width,q):
-                  if labels[r[0], r[1]] == FLAG:
-                     fifo.append(r)
-                     labels[r[0], r[1]] = current_label
+   """Get the amount of the area having the same gray scale
 
-      # Turn to next gray scale
-      start_index = stop_index
-   return result
+   Args:
+      file: bin file "The image represented by a binary file"
+      height: int "The height of the image"
+      width: int "The width of the image"
+
+   Returns:
+      1*256 array
+   """
+   def apply(self,file,height,width):
+      INIT = -1
+      scale_indices = []
+      current_scale = 0
+      image= np.fromfile(file,dtype='uint8')
+      total = height * width
+      self.height=height
+      self.width=width
+      results=[]
+
+      # Create a canva with an identical scale covered by an intial value
+      self.labels = np.full((height, width), INIT, np.int32)
+
+      reshaped_image = image.reshape(total)
+      # Create coordinates of every pixel.
+      pixels = np.mgrid[0:height, 0:width].reshape(2, -1).T
+
+      # Sort coordinates by gray scale
+      indices = np.argsort(reshaped_image)
+      self.sorted_image = reshaped_image[indices]
+      self.sorted_pixels = pixels[indices]
+
+      # Get the boundaries of pixels with different gray scales.
+      for i in range(total):
+         if self.sorted_image[i] <= current_scale:continue
+         if len(scale_indices)==0:
+            scale_indices.append([0,i])
+         else:
+            last_scale_indices=scale_indices[-1][1]
+            scale_indices.append([last_scale_indices,i])
+         current_scale=self.sorted_image[i]
+      final_scale_indices=scale_indices[-1][1]
+      scale_indices.append([final_scale_indices,total])
+
+      # Use multiprocessing to count areas
+      with Pool(len(os.sched_getaffinity(0))) as pool:
+         results=pool.map(self.count_areas, scale_indices)
+
+      # Aggregate all arrays in results
+      return [sum(x) for x in zip(*results)]
